52730fe47b13c9820ef9c8feb4222e68757ed82f
cyang19 <cyang@college.harvard.edu>
2024-02-18 10:46:49 -0500

    watch tut

--- progset.cpp
+++ progset.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
 
+
+
+//compiling with gcc : g++-13 (name.cpp) -o name ??
+
+using namespace std;
 
 int main()
 {   
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << std::endl;
     return 0;
+
+    ./randmst 0 numpoints numtrials dimension
 }
