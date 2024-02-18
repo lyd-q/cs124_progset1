208ea6839ea77b82cbcc9daa5c432593c7ccc565
Lydia Qin <lydiaqin@college.harvard.edu>
2024-02-18 12:52:09 -0500

    update

--- progset.cpp
+++ progset.cpp
@@ -4,8 +4,12 @@
 #include<string>
 
 
-
 //compiling with gcc : g++-13 (name.cpp) -o name ??
+//random number generation
+Mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_real_distribution<float> distribution(0.0, 1.0);
+//distribution(rng) gives random number from 0 to 1
+
 
 using namespace std;
 
