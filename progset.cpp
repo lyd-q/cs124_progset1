0f50abbdb97a44800eb86e61931f1003bb0e3f57
cyang19 <cyang@college.harvard.edu>
2024-02-18 12:57:08 -0500

    sdkjalkdjf

--- progset.cpp
+++ progset.cpp
@@ -3,7 +3,10 @@
 #include<vector>
 #include<string>
 
-
+mt19937 rng(chrono::stead_clock::now().time_since_epoch().count());
+// if you're testing change seed to constant so you can replicate rng(0)
+uniform_real_distribution<float> distribution(0,0, 1.0)
+//calling distribution(rng) gives random float 0 to 1
 
 //compiling with gcc : g++-13 (name.cpp) -o name ??
 
@@ -11,7 +14,8 @@ using namespace std;
 
 int main()
 {   
-    cout << "Hello World" << std::endl;
+    // << points in direction that information is flowing
+    cout << "Hello World" << endl;
     return 0;
 
     ./randmst 0 numpoints numtrials dimension
