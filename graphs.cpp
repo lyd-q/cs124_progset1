7895a1693f181867c22fe089655d6dbb74b38c50
Lydia Qin <lydiaqin@college.harvard.edu>
2024-02-19 14:05:54 -0500

    graphs updated

--- graphs.cpp
+++ graphs.cpp
@@ -1,41 +1,38 @@
-#include "graphs.h"
-
 #include <iostream>
 #include <vector>
 #include <string>
 #include <random>
 #include <cmath>
 #include <stdexcept>
+#include <cstdio>
+#include <cstdlib>
 #include <tuple>
 
 
 using namespace std;
 
 
-vector<vector<pair<int, float> > > make_graph(int n, int dim) {
+vector<tuple<int, int, float> > make_graph(int n, int dim) {
     //for random number generation
     mt19937 rng(time(nullptr));
     uniform_real_distribution<float> dist(0.0, 1.0);
 
-    vector<vector<pair<int, float> > > adj;
-    adj.resize(n);
+    //make vector of tuples
+    vector<tuple<int, int, float> > edges;
 
     // making first type graph (0-d)
     if (dim == 0) {
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
-                //get random edge weight
-                float w = dist(rng);
-                //make edges
-                //j = edge destination, w = edge weight
-                adj[i].push_back(make_pair(j, w)); 
-                adj[j].push_back(make_pair(i, w));
+                float weight = dist(rng);
+                tuple<int, int, float> e = make_tuple(i, j, weight);
+                edges.push_back(e);
             }
         }
     }
     else if (dim == 2 || dim == 3 || dim == 4) {
         //make a vector of points (positions randomly generated)
-        vector<vector<float>> points;
+        vector<vector<float> > points;
         for (int i = 0; i < n; i++) {
             vector<float> pt;
             for (int d = 0; d < dim; d++) {
@@ -43,15 +40,8 @@ vector<vector<pair<int, float> > > make_graph(int n, int dim) {
             }
             points.push_back(pt);
         }
-        //create edge list for the graph, vector version
-        //vector <vector<int, int, float> > edges;
-
-        // //create adjacency list, vertex indexed in order from points vector
-        // vector<vector<pair<int, float> > > adj;
-        // adj.resize(n);
         
-        //make vector of tuples
-        vector <tuple<int, int, float> > edges;
+
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
                 //get euclidean distance
@@ -60,9 +50,6 @@ vector<vector<pair<int, float> > > make_graph(int n, int dim) {
                     sum += pow(points[i][d] - points[j][d], 2);
                 }
                 float weight = sqrt(sum);
-                // //add to adjacency list as a pair of (vertex index, edge weight)
-                // adj[i].push_back(make_pair(j, weight)); 
-                // adj[j].push_back(make_pair(i, weight));
 
                 tuple<int, int, float> e = make_tuple(i, j, weight);
                 edges.push_back(e);
@@ -72,5 +59,14 @@ vector<vector<pair<int, float> > > make_graph(int n, int dim) {
     else {
         throw invalid_argument("Invalid dimension");
     }
-    return adj;
+    return edges;
 }
+
+// int main() {
+//     vector<tuple<int, int, float> > graph = make_graph(5, 4);
+//     for (int i = 0; i < graph.size(); i++) {
+//         auto [first, second, third] = graph[i];
+//         cout << "(" << first << ", " << second << "," << third << ")" << endl;   
+//     }
+// }
+
