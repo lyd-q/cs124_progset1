601d9b8e5bab41a9c5872affb203368311f85929
cyang19 <cyang@college.harvard.edu>
2024-02-18 21:52:33 -0500

    heap implemented

--- progset.cpp
+++ progset.cpp
@@ -3,22 +3,27 @@
 #include<vector>
 #include<string>
 
-<<<<<<< HEAD
 mt19937 rng(chrono::stead_clock::now().time_since_epoch().count());
 // if you're testing change seed to constant so you can replicate rng(0)
 uniform_real_distribution<float> distribution(0,0, 1.0)
 //calling distribution(rng) gives random float 0 to 1
-=======
->>>>>>> 208ea6839ea77b82cbcc9daa5c432593c7ccc565
 
 //compiling with gcc : g++-13 (name.cpp) -o name ??
 //random number generation
-Mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 uniform_real_distribution<float> distribution(0.0, 1.0);
 //distribution(rng) gives random number from 0 to 1
 
 
 using namespace std;
+// vector: dynamic list 
+// constructor, resize, push_back 
+
+//pair: pairs of values
+// make_pair, p.first, p.second
+
+
+
 
 int main()
 {   
