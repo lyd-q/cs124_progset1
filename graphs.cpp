b4b309db6e811169d08b6968e941a279ff2fee49
Lydia Qin <lydiaqin@college.harvard.edu>
2024-02-18 23:23:24 -0500

    commiting

--- graphs.cpp
+++ graphs.cpp
@@ -5,12 +5,14 @@
 #include <string>
 #include <random>
 #include <cmath>
+#include <stdexcept>
+
 
 using namespace std;
 
 
 // making first type graph (0-d)
-vector<vector<pair<int, float> > > make_0d_graph(int n) {
+vector<vector<pair<int, float> > > make_graph(int n, int dim) {
     //for random number generation
     mt19937 rng(time(nullptr));
     uniform_real_distribution<float> dist(0.0, 1.0);
@@ -18,161 +20,104 @@ vector<vector<pair<int, float> > > make_0d_graph(int n) {
     vector<vector<pair<int, float> > > adj;
     adj.resize(n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i+1; j < n; j++) {
-            //get random edge weight
-            float w = dist(rng);
-            //make edges
-            //j = edge destination, w = edge weight
-            adj[i].push_back(make_pair(j, w)); 
-		    adj[j].push_back(make_pair(i, w));
+    if (dim == 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                //get random edge weight
+                float w = dist(rng);
+                //make edges
+                //j = edge destination, w = edge weight
+                adj[i].push_back(make_pair(j, w)); 
+                adj[j].push_back(make_pair(i, w));
+            }
         }
+    }
+    else if (dim == 1 || dim == 2 || dim == 3) {
+        //make a vector of points (positions randomly generated)
+        vector<vector<float>> points;
+        for (int i = 0; i < n; i++) {
+            vector<float> pt;
+            for (int d = 0; d < dim; d++) {
+                pt.push_back(dist(rng));
+            }
+            points.push_back(pt);
+        }
+
+        //create adjacency list, vertex indexed in order from points vector
+        vector<vector<pair<int, float> > > adj;
+        adj.resize(n);
+        
+        vector <tuple<int, int, float>> edges;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                float sum = 0;
+                for (int d = 0; d < dim; d++) {
+                    sum += pow(points[i][d] - points[j][d], 2);
+                }
+                float weight = sqrt(sum);
+                //add to adjacency list as a pair of (vertex index, edge weight)
+                adj[i].push_back(make_pair(j, weight)); 
+                adj[j].push_back(make_pair(i, weight));
+
+                // make a vector list for edges
+                //edges.push_back(make_tuple(i, j, weight));
+            }
+        }
+    }
+    else {
+        throw invalid_argument("Invalid dimension");
     }
     return adj;
 }
 
+// //make multidimensional graph
+// vector<vector<pair<int, float> > > make_multdim_graph(int n, int dim) {
+//     //for random number generation
+//     mt19937 rng(time(nullptr));
+//     uniform_real_distribution<float> dist(0.0, 1.0);
 
-//make second type 2d-graph
-vector<vector<pair<int, float> > > make_2d_graph(int n) {
-    //for random number generation
-    mt19937 rng1(time(nullptr));
-    mt19937 rng2(time(nullptr));
-    uniform_real_distribution<float> dist(0.0, 1.0);
-
-    //make a vector of points (positions randomly generated)
-    vector<pair<float, float> > points;
-    for (int i = 0; i < n; i++) {
-        float x = dist(rng1);
-        float y = dist(rng2);
-        points.push_back(make_pair(x, y));
-    }
-    print_2d_points(points);
-    //create adjacency list, vertex indexed in order from points vector
-    vector<vector<pair<int, float> > > adj;
-    adj.resize(n);
+//     //make a vector of points (positions randomly generated)
+//     vector<vector<float>> points;
+//     for (int i = 0; i < n; i++) {
+//         vector<float> pt;
+//         for (int d = 0; d < dim; d++) {
+//             pt.push_back(dist(rng));
+//         }
+//         points.push_back(pt);
+//     }
     
-    for (int i = 0; i < n; i++){
-        float x1 = points[i].first;
-        float y1 = points[i].second;
-
-        for (int j = i+1; j < n; j++) {
-            //calculate edge weights using euclidean distance
-            float x2 = points[j].first;
-            float y2 = points[j].second;
-            float w = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-
-            //add to adjacency list as a pair of (vertex index, edge weight)
-            adj[i].push_back(make_pair(j, w)); 
-		    adj[j].push_back(make_pair(i, w));
-        }
-    }
-    return adj;
-}
-
-
-//make 3d-graph
-vector<vector<pair<int, float> > > make_3d_graph(int n) {
-    //for random number generation
-    mt19937 rng1(time(nullptr));
-    mt19937 rng2(time(nullptr));
-    mt19937 rng3(time(nullptr));
-    uniform_real_distribution<float> dist(0.0, 1.0);
-
-    //make a vector of points (positions randomly generated)
-    vector<vector<float>> points;
-    for (int i = 0; i < n; i++) {
-        float x = dist(rng1);
-        float y = dist(rng2);
-        float z = dist(rng3);
-        vector<float> pt{x, y, z};
-        points.push_back(pt);
-    }
-    print_3d_points(points);
-
-    //create adjacency list, vertex indexed in order from points vector
-    vector<vector<pair<int, float> > > adj;
-    adj.resize(n);
+//     //create adjacency list, vertex indexed in order from points vector
+//     vector<vector<pair<int, float> > > adj;
+//     adj.resize(n);
     
-    for (int i = 0; i < n; i++){
-        float x1 = points[i][0];
-        float y1 = points[i][1];
-        float z1 = points[i][2];
-
-        for (int j = i+1; j < n; j++) {
-            //calculate edge weights using euclidean distance
-            float x2 = points[j][0];
-            float y2 = points[j][1];
-            float z2 = points[j][2];
-            float w = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + + pow(z2 - z1, 2));
-
-            //add to adjacency list as a pair of (vertex index, edge weight)
-            adj[i].push_back(make_pair(j, w)); 
-		    adj[j].push_back(make_pair(i, w));
-        }
-    }
-    return adj;
-}
-
-//make 4d-graph
-vector<vector<pair<int, float> > > make_4d_graph(int n) {
-    //for random number generation
-    mt19937 rng0(time(nullptr));
-    mt19937 rng1(time(nullptr));
-    mt19937 rng2(time(nullptr));
-    mt19937 rng3(time(nullptr));
-    uniform_real_distribution<float> dist(0.0, 1.0);
-
-    //make a vector of points (positions randomly generated)
-    vector<vector<float>> points;
-    for (int i = 0; i < n; i++) {
-        float w = dist(rng0);
-        float x = dist(rng1);
-        float y = dist(rng2);
-        float z = dist(rng3);
-        vector<float> pt{w, x, y, z};
-        points.push_back(pt);
-    }
-    print_4d_points(points);
-
-    //create adjacency list, vertex indexed in order from points vector
-    vector<vector<pair<int, float> > > adj;
-    adj.resize(n);
-    
-    for (int i = 0; i < n; i++){
-        float w1 = points[i][0];
-        float x1 = points[i][1];
-        float y1 = points[i][2];
-        float z1 = points[i][3];
-
-        for (int j = i+1; j < n; j++) {
-            //calculate edge weights using euclidean distance
-            float w2 = points[j][0];
-            float x2 = points[j][1];
-            float y2 = points[j][2];
-            float z2 = points[j][3];
-            float weight = sqrt(pow(w2 - w1, 2) + pow(x2 - x1, 2) + pow(y2 - y1, 2) + + pow(z2 - z1, 2));
-
-            //add to adjacency list as a pair of (vertex index, edge weight)
-            adj[i].push_back(make_pair(j, weight)); 
-		    adj[j].push_back(make_pair(i, weight));
-        }
-    }
-    return adj;
-}
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i+1; j < n; j++) {
+//             float sum = 0;
+//             for (int d = 0; d < dim; d++) {
+//                 sum += pow(points[i][d] - points[j][d], 2);
+//             }
+//             float weight = sqrt(sum);
+//             //add to adjacency list as a pair of (vertex index, edge weight)
+//             adj[i].push_back(make_pair(j, weight)); 
+//             adj[j].push_back(make_pair(i, weight));
+//         }
+//     }
+//     return adj;
+// }
 
 
 
 // //functions to print (for testing)
 // //print adjacency list of graph
-// void print_graph (vector<vector<pair<int, float> > > adj) {
-//     cout << "Graph:" << endl;
-//     for (int i = 0; i < adj.size(); i++) {
-//         for (int j = 0; j < adj[i].size(); j++) {
-//             cout << "(" << adj[i][j].first << ", " << adj[i][j].second << ")";
-//         }
-//         cout << endl;
-//     }
-// }
+void print_graph (vector<vector<pair<int, float> > > adj) {
+    cout << "Graph:" << endl;
+    for (int i = 0; i < adj.size(); i++) {
+        for (int j = 0; j < adj[i].size(); j++) {
+            cout << "(" << adj[i][j].first << ", " << adj[i][j].second << ")";
+        }
+        cout << endl;
+    }
+}
 
 // //print 2d points vector
 // void print_2d_points (vector<pair<float, float> > points) {
@@ -199,4 +144,82 @@ vector<vector<pair<int, float> > > make_4d_graph(int n) {
 //         cout << "(" << points[i][0] << ", " << points[i][1] << ", " << points[i][2] << ", " << points[i][3] << ")";
 //     }
 //     cout << endl;
+// }
+
+// //make second type 2d-graph
+// vector<vector<pair<int, float> > > make_2d_graph(int n) {
+//     //for random number generation
+//     mt19937 rng1(time(nullptr));
+//     mt19937 rng2(time(nullptr));
+//     uniform_real_distribution<float> dist(0.0, 1.0);
+
+//     //make a vector of points (positions randomly generated)
+//     vector<pair<float, float> > points;
+//     for (int i = 0; i < n; i++) {
+//         float x = dist(rng1);
+//         float y = dist(rng2);
+//         points.push_back(make_pair(x, y));
+//     }
+//     //create adjacency list, vertex indexed in order from points vector
+//     vector<vector<pair<int, float> > > adj;
+//     adj.resize(n);
+    
+//     for (int i = 0; i < n; i++){
+//         float x1 = points[i].first;
+//         float y1 = points[i].second;
+
+//         for (int j = i+1; j < n; j++) {
+//             //calculate edge weights using euclidean distance
+//             float x2 = points[j].first;
+//             float y2 = points[j].second;
+//             float w = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+//             //add to adjacency list as a pair of (vertex index, edge weight)
+//             adj[i].push_back(make_pair(j, w)); 
+// 		    adj[j].push_back(make_pair(i, w));
+//         }
+//     }
+//     return adj;
+// }
+
+
+// //make 3d-graph
+// vector<vector<pair<int, float> > > make_3d_graph(int n) {
+//     //for random number generation
+//     mt19937 rng1(time(nullptr));
+//     mt19937 rng2(time(nullptr));
+//     mt19937 rng3(time(nullptr));
+//     uniform_real_distribution<float> dist(0.0, 1.0);
+
+//     //make a vector of points (positions randomly generated)
+//     vector<vector<float>> points;
+//     for (int i = 0; i < n; i++) {
+//         float x = dist(rng1);
+//         float y = dist(rng2);
+//         float z = dist(rng3);
+//         vector<float> pt{x, y, z};
+//         points.push_back(pt);
+//     }
+//     //create adjacency list, vertex indexed in order from points vector
+//     vector<vector<pair<int, float> > > adj;
+//     adj.resize(n);
+    
+//     for (int i = 0; i < n; i++){
+//         float x1 = points[i][0];
+//         float y1 = points[i][1];
+//         float z1 = points[i][2];
+
+//         for (int j = i+1; j < n; j++) {
+//             //calculate edge weights using euclidean distance
+//             float x2 = points[j][0];
+//             float y2 = points[j][1];
+//             float z2 = points[j][2];
+//             float w = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + + pow(z2 - z1, 2));
+
+//             //add to adjacency list as a pair of (vertex index, edge weight)
+//             adj[i].push_back(make_pair(j, w)); 
+// 		    adj[j].push_back(make_pair(i, w));
+//         }
+//     }
+//     return adj;
 // }
--- main.cpp
+++ main.cpp
@@ -9,137 +9,146 @@
 
 using namespace std;
 
-struct heap {
-	//fields 
-	vector<int> arr;
-
-    //constructor
-    heap() {
-        arr = vector<pair<float, int>>();
-    }
-
-    heap(int n){
-        arr.resize(n);
-    }
-
-    //methods 
-
-    //maxheapify
-    void max_heapify(int node){
-        int largest = 0;
-        int l = (node)*2;
-        int r = (node)*2 + 1;
-
-        // HOW TO CHECK IF L EXISTS?
-        if (arr[l-1] > arr[node-1] && arr.size() >= l){
-            largest = l;
-        }
-        else{
-            largest = node;
-        }
-
-        if (arr.size() > r && arr[r-1] > arr[largest- 1]){
-            largest = r;
-        }
-
-        // RECUIRSIVE CALL? ALSO SWAP?
-        if (largest != node){
-            // i am swapping 
-            int temp = 0;
-            temp = arr[node-1];
-            arr[node-1] = arr[largest - 1];
-            arr[largest-1] = temp;
-
-            max_heapify(largest);
-        }
-    }
-
-    //buildheap
-
-    // why are we giving it an array here? okay so am i assigning the arr 
-    //to be the result of this build/
-    // how to call max_heapify on array?  
-    void build(vector<int> A){
-        arr = A;
-        for(int i = floor(A.size()/2) - 1; i >= 0; i--){
-            max_heapify(i);
-        }
-    }
-
-    // peek
-    int peek(){
-        return arr[0];
-    }
-
-    //extract max 
-    // same thing here, how am i calling heapify on itself?
-    int extract_max(){
-        int max = arr[0];
-        arr[0] = arr[arr.size()];
-        arr.resize(arr.size()-1);
-        max_heapify(1); // im calling it on the root node number
-
-    }
-
-    // insert
-    void insert(int v){
-        arr.resize(arr.size()+1);
-        arr[arr.size()] = v;
-        int N = arr.size();
-        while (N != 1 && arr[floor(N/2)-1] < arr[N-1]){
-            int p =floor(N/2)-1;
-            int temp = arr[p];
-            arr[p] = arr[N-1];
-            arr[N-1] = temp;
-            N = p;
-        }
-    }
-
-    bool empty() {
-        return arr.empty();
-    }
-};
-
-int main(int argc, char** argv)
-{   
+int main() {
     //get input 
     int flag, numpoints, numtrials, dimension;
     cin >> flag >> numpoints >> numtrials >> dimension;
 
-    vector<vector<pair<int, float> > > test_graph = make_4d_graph(3);
+    //adj list version
+    vector<vector<pair<int, float> > > test_graph = make_graph(3, 2);
     print_graph(test_graph);
 
-    // get input argument 
-    int n = stoi(argv[1]);
-
-    //now i run prims
-    heap myheap;
-    //list of distances
-    vector<float> dist(n, 1e9);
-
-    //visited set
-    vector<bool> vis(n, false);
-
-    //set S 
-    vector<int> S(0, 0);
-    //unordered set.? 
-
-    // start at s apparently 
-    dist[0] = 0;
-    myheap.insert(0);
-
-    while (!myheap.empty()) {
-        int u = ;
-        S.insert(u);
-
-        for (auto p: adj[u] && p) {
-
-        }
-    }
-
-
-
-
-
-    return 0;
+    //edge version
+    
 }
+
+
+// struct heap {
+// 	//fields 
+// 	vector<int> arr;
+
+//     //constructor
+//     heap() {
+//         arr = vector<pair<float, int>>();
+//     }
+
+//     heap(int n){
+//         arr.resize(n);
+//     }
+
+//     //methods 
+
+//     //maxheapify
+//     void max_heapify(int node){
+//         int largest = 0;
+//         int l = (node)*2;
+//         int r = (node)*2 + 1;
+
+//         // HOW TO CHECK IF L EXISTS?
+//         if (arr[l-1] > arr[node-1] && arr.size() >= l){
+//             largest = l;
+//         }
+//         else{
+//             largest = node;
+//         }
+
+//         if (arr.size() > r && arr[r-1] > arr[largest- 1]){
+//             largest = r;
+//         }
+
+//         // RECUIRSIVE CALL? ALSO SWAP?
+//         if (largest != node){
+//             // i am swapping 
+//             int temp = 0;
+//             temp = arr[node-1];
+//             arr[node-1] = arr[largest - 1];
+//             arr[largest-1] = temp;
+
+//             max_heapify(largest);
+//         }
+//     }
+
+//     //buildheap
+
+//     // why are we giving it an array here? okay so am i assigning the arr 
+//     //to be the result of this build/
+//     // how to call max_heapify on array?  
+//     void build(vector<int> A){
+//         arr = A;
+//         for(int i = floor(A.size()/2) - 1; i >= 0; i--){
+//             max_heapify(i);
+//         }
+//     }
+
+//     // peek
+//     int peek(){
+//         return arr[0];
+//     }
+
+//     //extract max 
+//     // same thing here, how am i calling heapify on itself?
+//     int extract_max(){
+//         int max = arr[0];
+//         arr[0] = arr[arr.size()];
+//         arr.resize(arr.size()-1);
+//         max_heapify(1); // im calling it on the root node number
+
+//     }
+
+//     // insert
+//     void insert(int v){
+//         arr.resize(arr.size()+1);
+//         arr[arr.size()] = v;
+//         int N = arr.size();
+//         while (N != 1 && arr[floor(N/2)-1] < arr[N-1]){
+//             int p =floor(N/2)-1;
+//             int temp = arr[p];
+//             arr[p] = arr[N-1];
+//             arr[N-1] = temp;
+//             N = p;
+//         }
+//     }
+
+//     bool empty() {
+//         return arr.empty();
+//     }
+// };
+
+// int main(int argc, char** argv)
+// {   
+//     //get input 
+//     int flag, numpoints, numtrials, dimension;
+//     cin >> flag >> numpoints >> numtrials >> dimension;
+
+//     vector<vector<pair<int, float> > > test_graph = make_4d_graph(3);
+//     print_graph(test_graph);
+
+//     // get input argument 
+//     int n = stoi(argv[1]);
+
+//     //now i run prims
+//     heap myheap;
+//     //list of distances
+//     vector<float> dist(n, 1e9);
+
+//     //visited set
+//     vector<bool> vis(n, false);
+
+//     //set S 
+//     vector<int> S(0, 0);
+//     //unordered set.? 
+
+//     // start at s apparently 
+//     dist[0] = 0;
+//     myheap.insert(0);
+
+//     while (!myheap.empty()) {
+//         int u = ;
+//         S.insert(u);
+
+//         for (auto p: adj[u] && p) {
+
+//         }
+//     }
+//     return 0;
+// }
